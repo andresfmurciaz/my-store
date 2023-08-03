7f532346577019272f9e54dfe3bad29241e78812
Andres Felipe Murcia Zapata <101027787+andresfmurciaz@users.noreply.github.com>
2023-08-03 17:57:34 -0500

    eventos scroll keyup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,26 @@ export class AppComponent {
     name:'camila',
     age:19
   }
+
+  actiDesa(){
+  this.estadoButt=!this.estadoButt
+
+  }
+
+
+  aumentarEdad(){
+    this.person.age= this.person.age+1;
+  }
+
+  leerScroll(event:Event){
+const elemnto = event.target as HTMLElement;
+console.log(elemnto)
+}
+
+leerInput(event:Event){
+  const elemnto = event.target as HTMLInputElement;
+this.person.name=elemnto.value;
+console.log(elemnto.value)
+}
+
 }
